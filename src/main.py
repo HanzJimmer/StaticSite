6c43e24c69c139f4788aa55211306409f971d3a2
HanzJimmer <cmealey4@gmail.com>
2026-05-16 22:58:42 -0600

    added generate_page fucntion. currently working on recursive generation

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import os
 import shutil
+from helpfunctions import markdown_to_html_node
 
 def main():
     static_path = "static"
     public_path = "public"
     copy_static(static_path, public_path)
+    
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 def copy_static(src, dest):
     if os.path.exists(dest):
@@ -24,5 +27,28 @@ def copy_static(src, dest):
 def extract_title(markdown):
     if not markdown.startswith("# "):
         raise Exception
+    header = (markdown.split("\n"))[0]
+    header = header.removeprefix("# ")
+    return header
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    read_file = None
+    with open(from_path) as f:
+        read_file = f.read()
+    template = None
+    with open(template_path) as f:
+        template = f.read()
+    my_html_string = markdown_to_html_node(read_file).to_html()
+    print(read_file)
+    head = extract_title(read_file)
+
+    template = template.replace("{{ Title }}", head)
+    template = template.replace("{{ Content }}", my_html_string)
+    if (not os.path.exists(dest_path)):
+        os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    
+    with open(dest_path, "w") as f:
+        f.write(template)
 
 main()
